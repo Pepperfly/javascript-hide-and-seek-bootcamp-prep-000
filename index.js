a2f6a1866d89725d47ab2c909086f36b44da8fad
Jeff Carter <jeff@pepperfly.com>
2018-09-20 15:20:46 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ function getFirstSelector(selector) {
 
 
 
+// nestedTarget()...
 function nestedTarget() {
   return document.getElementById('nested').getElementsByClassName('target')[0];
 }
+
+/*Learn.io solution...
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+*///
 
 
 
