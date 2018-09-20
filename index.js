2cddee251acb3b8051bd48e507c96f69a187bb8f
Jeff Carter <jeff@pepperfly.com>
2018-09-20 15:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+
+
+function increaseRankBy(n) {
+  var rankedLis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedLis.length; i++) {
+    rankedLis[i].innerHTML = (parseInt(rankedLis[i].innerHTML) + n).toString();
+  }
+}
+
+
+
+function deepestChild() {
+  var node = document.getElementById('grand-node');
+  node = node.querySelectorAll('*');
+  return node[node.length - 1];
+}
