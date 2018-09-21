3272d16634b4e2bf384bb077b8ead684376d052b
Jeff Carter <jeff@pepperfly.com>
2018-09-21 21:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+<<<<<<< HEAD
+=======
+
+
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target');
+}
+
+
+
+function increaseRankBy(n) {
+  var rankedLis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedLis.length; i++) {
+    rankedLis[i].innerHTML = (parseInt(rankedLis[i].innerHTML) + 1).toString();
+  }
+}
+
+
+
+function deepestChild() {
+  // get to the node div#grand-node
+  var grandNode = document.getElementById('grand-node');
+  // we know all are divs at each level, so query 'em all and determine how many were counted
+  var deepDiv = grandNode.querySelectorAll('div').length;
+  return deepDiv;
+  
+}
+
+
+>>>>>>> 80f4c9549a6820b421823dffec045e8eb4e966bd
 
 
 
